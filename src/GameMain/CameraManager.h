d6710dd2179de1d90cb9985b2fefce95e3b91b64
401-35\koyo <kobe-license@kobe-tech.ac.jp>
2025-11-24 09:12:47 +0900

    カメラマネージャー変更

--- src/GameMain/CameraManager.h
+++ src/GameMain/CameraManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "DxLib.h"
 #include <cmath>
 
 class Camera2D
@@ -10,6 +11,7 @@ class Camera2D
 
 	// 倍率
 	float m_zoom = 1.0f;
+    const float ZOOM_SPEED = 0.1f;
 
 	// 追従値
 	bool m_follow = false;
@@ -24,12 +26,85 @@ class Camera2D
 
 public:
 
-	// 追尾
+    // ===== コンストラクタ =====
+    Camera2D() {}
 
-	// アップ
+    void Move(float arg_dx, float arg_dy) {
+        m_camX += arg_dx;
+        m_camY += arg_dy;
+    }
 
-	// クローズ
+    void UpZoom(float arg_dz) {
+        m_zoom += ZOOM_SPEED;
+        if (m_zoom >= arg_dz)
+        {
+            m_zoom = arg_dz;
+        }
+    }
+    void SetZoom(float arg_z) {
+        m_zoom = arg_z;
+    }
 
-	// シェイク
+    float GetZoom() const { return m_zoom; }
 
+    // ===== 位置設定 =====
+    void SetPos(float arg_x, float arg_y) {
+        m_camX = arg_x;
+        m_camY = arg_y;
+    }
+
+    float GetX() const { return m_camX; }
+    float GetY() const { return m_camY; }
+
+    void Follow(float arg_targetX, float arg_targetY, float arg_lerp = 0.1f) {
+        m_follow = true;
+        m_targetX = arg_targetX;
+        m_targetY = arg_targetY;
+        m_lerp = arg_lerp;
+    }
+
+    void StopFollow() {
+        m_follow = false;
+    }
+
+    void Shake(float arg_power, int arg_time) {
+        m_shakePower = arg_power;
+        m_shakeTime = arg_time;
+    }
+
+    void Update() {
+
+        // 追従処理
+        if (m_follow) {
+            m_camX += (m_targetX - m_camX) * m_lerp;
+            m_camY += (m_targetY - m_camY) * m_lerp;
+        }
+
+        // シェイク処理
+        if (m_shakeTime > 0) {
+            m_camX += (rand() % 1000 / 1000.0f - 0.5f) * m_shakePower;
+            m_camY += (rand() % 1000 / 1000.0f - 0.5f) * m_shakePower;
+            m_shakeTime--;
+        }
+    }
+
+    // 画面座標変換
+    void WorldToScreen(float worldX, float worldY, int& outX, int& outY) const {
+        outX = (int)((worldX - m_camX) * m_zoom);
+        outY = (int)((worldY - m_camY) * m_zoom);
+    }
+
+    // 描画
+    void DrawSprite(int x, int y, int graphHandle) const {
+        int w, h;
+        GetGraphSize(graphHandle, &w, &h);
+
+        int sx = (int)((x - m_camX) * m_zoom);
+        int sy = (int)((y - m_camY) * m_zoom);
+
+        int ex = sx + (int)(w * m_zoom);
+        int ey = sy + (int)(h * m_zoom);
+
+        DrawExtendGraph(sx, sy, ex, ey, graphHandle, TRUE);
+    }
 };
