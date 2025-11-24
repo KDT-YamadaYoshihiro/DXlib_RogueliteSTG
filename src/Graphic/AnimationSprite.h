29ca4f6b24768294eb649e20f8bf7db5b1eddf02
401-35\koyo <kobe-license@kobe-tech.ac.jp>
2025-11-24 12:16:39 +0900

    レンダークラス作成

--- src/Graphic/AnimationSprite.h
+++ src/Graphic/AnimationSprite.h
@@ -1,1 +1,61 @@
 #pragma once
+#include "DxLib.h"
+#include "Render.h"
+
+class AnimationSprite
+{
+	int image;
+	int frameWidth;
+	int frameHeight;
+	int frameCount;
+	float frameTime;
+	float timer = 0.0f;
+	int currentFrame = 0;
+
+	bool loop = true;
+	bool playing = true;
+
+public:
+
+	AnimationSprite(int arg_image, int arg_frameW, int arg_frameH, int arg_frames, float fps = 8.0f, bool loopflag = true)
+		:image(arg_image),
+		frameWidth(arg_frameW),
+		frameHeight(arg_frameH),
+		frameCount(arg_frames),
+		loop(loopflag)
+	{
+		frameTime = 1.0f / fps;
+	}
+
+	// アニメ更新
+	void Update(float delta)
+	{
+
+		if (!playing)
+		{
+			return;
+		}
+
+		timer += delta;
+		currentFrame++;
+
+		if (currentFrame >= frameCount)
+		{
+			if (loop)
+			{
+				currentFrame = 0;
+			}
+			else
+			{
+				currentFrame = frameCount - 1;
+				playing = false;
+
+			}// if(loop)
+
+		} // if (currentFrame >= frameCount)
+
+
+	}
+
+
+};
--- src/Graphic/Render.h
+++ src/Graphic/Render.h
@@ -1,27 +1,58 @@
 #pragma once
-#include "CameraManager.h"
+#include "DxLib.h"
+#include <memory>
+
+#include "../GameMain/CameraManager.h"
 
 class Render
 {
 
-	// 座標
-
-	// 描画画像位置
-
-	// 倍率
-
-	// 透明度
+	std::shared_ptr<Camera2D> camera;
 
 public:
+	
+	Render(std::shared_ptr<Camera2D> arg_camera):camera(arg_camera){}
 
-	// 座標位置調整（カメラに合わせる）
+	// 左上基準
+    void DrawSprite(int arg_x, int arg_y, int arg_graph)
+    {
+        int w, h;
+        GetGraphSize(arg_graph, &w, &h);
 
-	// 描画
+        int sx = (int)((arg_x - camera->GetX()) * camera->GetZoom());
+        int sy = (int)((arg_y - camera->GetY()) * camera->GetZoom());
+        int ex = sx + (int)(w * camera->GetZoom());
+        int ey = sy + (int)(h * camera->GetZoom());
 
-	// 倍率操作描画
+        DrawExtendGraph(sx, sy, ex, ey, arg_graph, TRUE);
+    }
 
-	// 透明度操作描画
+    // ===== 画像（中心基準） =====
+    void DrawSpriteCenter(int arg_x, int arg_y, int arg_graph)
+    {
+        int w, h;
+        GetGraphSize(arg_graph, &w, &h);
 
-	// 倍率・透明度操作描画
+        int left = arg_x - w / 2;
+        int top = arg_y - h / 2;
 
+        DrawSprite(left, top, arg_graph);
+    }
+
+    // ===== 四角形描画（デバッグ用） =====
+    void DrawBox(int arg_x, int arg_y, int arg_w, int arg_h, unsigned int arg_color, bool arg_fill = false)
+    {
+        int sx1 = (int)((arg_x - camera->GetX()) * camera->GetZoom());
+        int sy1 = (int)((arg_y - camera->GetY()) * camera->GetZoom());
+        int sx2 = sx1 + (int)(arg_w * camera->GetZoom());
+        int sy2 = sy1 + (int)(arg_h * camera->GetZoom());
+
+        DrawBox(sx1, sy1, sx2, sy2, arg_color, arg_fill);
+    }
+
+    // ===== テキスト描画（カメラに影響しない UI 用） =====
+    void DrawUI(int arg_x, int arg_y, const char* arg_text, unsigned int arg_color)
+    {
+        DrawString(arg_x, arg_y, arg_text, arg_color);
+    }
 };
